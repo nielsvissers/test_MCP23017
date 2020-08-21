0b6677209f0c04c59f7198e41c2caad5993af43e
Niels Vissers <niels.vissers@digipolis.be>
2020-08-22 00:12:34 +0200

    Update MCP23017.cpp

--- MCP23017.cpp
+++ MCP23017.cpp
@@ -21,13 +21,15 @@ int main()
 		return -1;
 	}
 	
-	int addr = 0x20;          //<<<<<The I2C address of the slave
+	int addr = MCP23017_ADDRESS;          //<<<<<The I2C address of the slave
 	if (ioctl(file_i2c, I2C_SLAVE, addr) < 0)
 	{
 		printf("Failed to acquire bus access and/or talk to slave.\n");
 		//ERROR HANDLING; you can check errno to see what went wrong
 		return -1;
 	}
+
+    init(file_i2c);
 	
 	while(true)
     {
@@ -48,4 +50,27 @@ for (i=0;i<60;i++)
 printf("\n");
 	}
 }
+}
+void writeRegister(int regAddress, int value)
+{
+	//----- WRITE BYTES -----
+	buffer[0] = regAddress;
+	buffer[1] = value;
+	length = 2;			//<<< Number of bytes to write
+	if (write(file_i2c, buffer, length) != length)		//write() returns the number of bytes actually written, if it doesn't match then an error occurred (e.g. no response from the device)
+	{
+		/* ERROR HANDLING: i2c transaction failed */
+		printf("Failed to write to the i2c bus.\n");
+	}
+}
+void init(int file_i2c)
+{
+    //set bank A as input
+    writeRegister(file_i2c, MCP23017_IODIRA, INPUT);
+    //set bank B as input
+    writeRegister(file_i2c, MCP23017_IODIRB, INPUT);
+    //set pullup bank A
+    writeRegister(file_i2c, MCP23017_GPPUA, HIGH);
+    //set pullup bank B
+    writeRegister(file_i2c, MCP23017_GPPUB, HIGH);
 }
