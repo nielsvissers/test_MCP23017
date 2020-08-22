0e0904b6f629dd61d04700ff35d867ebd8452374
Niels Vissers <niels.vissers@digipolis.be>
2020-08-22 16:50:31 +0200

    Update expander_lib.cpp

--- expander_lib.cpp
+++ expander_lib.cpp
@@ -137,7 +137,7 @@ int read_mcp23017(mcp23017 expander, int reg)
    int value,fd;
    fd = open_mcp23017(expander);
    unsigned char buffer[2]={0},
-   write(0x13);
+   write(fd, 0x13, 1);
    if((value = read(fd, buffer ,1)) < 0)
      {
      printf("Failed to read from the i2c bus\n");
